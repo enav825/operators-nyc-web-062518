64ce512fb67609c11a668cb33308cea139c0d113
Ena Anicama <ena.v825@gmail.com>
2018-07-08 17:21:38 -0400

    complete lab

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
+require 'pry'
+
+
 def unsafe?(speed)
-
+	if speed == 50
+		false
+	else speed < 40 || speed > 60
+		true
+	end
 end
-
 
 
 def not_safe?(speed)
-	
+	speed = 95 ? speed < 40 || speed > 60 : speed == 50
 end
-	
-
-
